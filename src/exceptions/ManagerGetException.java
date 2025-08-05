7419585da4e033df7225e8e333e7095e00123810
Kir <tcherepanov.kir@gmail.com>
2025-08-05 11:10:06 +0300

    [sprint-9]: fix style error

--- src/exceptions/ManagerGetException.java
+++ src/exceptions/ManagerGetException.java
@@ -3,5 +3,7 @@ package exceptions;
 import java.io.IOException;
 
 public class ManagerGetException extends IOException {
-    public ManagerGetException(String message) { super(message); }
+    public ManagerGetException(String message) {
+        super(message);
+    }
 }
